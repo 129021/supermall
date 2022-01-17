01925a4515fe0bb971e0cec4e4c7f732e4b9cd82
129021 <wangjianqin1@yahoo.com>
2022-01-17 19:34:35 +0800

    not work today

--- src/components/content/goods/GoodsList.vue
+++ src/components/content/goods/GoodsList.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="goods">
     <goods-list-item v-for="item in goods" :goods-item="item" />
+    <!-- <goods-list-item v-for="item in goods" :goods-item="item" /> -->
+
+  
+
   </div>
 </template>
 
